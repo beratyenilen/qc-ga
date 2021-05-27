e09aa7eb02600ed4454276ca49a6dce50fad9414
Tom Rindell <tomoskar@protonmail.com>
2021-05-27 12:57:43 +0300

    Refactored transpile code

--- qiskit_transpiler/transpiled_initialization_circuits.py
+++ qiskit_transpiler/transpiled_initialization_circuits.py
@@ -18,86 +18,101 @@ from qiskit.circuit.library import Permutation
 from qiskit.transpiler import PassManager, CouplingMap, Layout
 from qiskit.transpiler.passes import BasicSwap, LayoutTransformation, RemoveFinalMeasurements
 
+#   Computes the fidelities for transpiled circuits
+def getFidelities(circs, machine_simulator, desired_vector):
+    fidelities = []
+    #n_shots = 20
+    n_iter = len(circs)
+    pad_vectors = []    #   List of perm_aug_desired_vectors for every circ
+    for i in range(n_iter):
+        
+        qubit_pattern = list(circs[i]._layout.get_virtual_bits().values()) # How virtual bits map to physical bits
+        n_phys = len(qubit_pattern) # n of physical bits
+
+        #   Computing the permutations done by
+        #   transpile function
+        perm = [0,1,2,3,4]
+        for op, qubits, clbits in circs[i].data:
+            if op.name == 'measure':
+                a = perm.index(clbits[0].index)
+                b = perm[qubits[0].index]
+                perm[qubits[0].index] = clbits[0].index
+                perm[a] = b
+                print("q: "+str(qubits[0].index))
+                print("c: "+str(clbits[0].index))
+                print(perm)
+        circs[i].remove_final_measurements()
+        circs[i].save_density_matrix()
+        
+        qubit_pattern = perm
+
+        aug_desired_vector = desired_vector
+
+        for k in range(n_phys-n):
+            aug_desired_vector = np.kron([1,0],aug_desired_vector)
+
+
+        perm_circ = Permutation(n_phys, qubit_pattern) # Creating a circuit for qubit mapping
+        perm_unitary = Operator(perm_circ) # Matrix for the previous circuit
+
+        perm_aug_desired_vector = perm_unitary.data @ aug_desired_vector
+        pad_vectors.append(perm_aug_desired_vector)
+
+    for i in range(n_iter):
+        result = execute(circs[i],machine_simulator,shots=1).result()
+        noisy_dens_matr = result.data()['density_matrix']
+        fid = state_fidelity(pad_vectors[i],noisy_dens_matr)
+        fidelities.append(fid)
+    return fidelities
+
+#   Generate a random desired_vector
+def randomDV():
+    desired_vector = np.random.rand(2**n)+1j*np.random.rand(2**n)
+    desired_vector = desired_vector/np.linalg.norm(desired_vector)
+    return desired_vector
+
+
+#   Generates n_iter amount of circuits for fake_machine
+#   Returns lists for circuits and for depths
+#
+#   Measurement gates are added to keep track of the permutations
+def genCircs(n, fake_machine, desired_vector, n_iter=10, optimization_level=2):
+    qr = QuantumRegister(n)
+    cr = ClassicalRegister(n)
+    init_circ = QuantumCircuit(qr, cr)
+
+    init_circ.initialize(desired_vector, qr)
+    init_circ.barrier()
+    init_circ.measure(qr, cr)
+
+    circs = []
+    depths = []
+
+    for _ in range(n_iter):
+        new_circ = transpile(init_circ,fake_machine,optimization_level=optimization_level)
+        circs.append(new_circ)
+        depths.append(new_circ.depth())
+    return circs, depths
+
+
 machine_simulator = Aer.get_backend('qasm_simulator')
 fake_machine = FakeAthens()
 n = 4
 
-desired_vector = np.random.rand(2**n)+1j*np.random.rand(2**n)
-desired_vector = desired_vector/np.linalg.norm(desired_vector)
-
+desired_vector = randomDV()
 print(desired_vector)
 print(np.linalg.norm(desired_vector))
 
+circs, depths = genCircs(n, fake_machine, desired_vector)
 
-qr = QuantumRegister(n)
-cr = ClassicalRegister(n)
-init_circ = QuantumCircuit(qr, cr)
-
-init_circ.initialize(desired_vector, qr)
-init_circ.barrier()
-init_circ.measure(qr, cr)
-
-circs = []
-depths = []
-
-n_iter = 10
-
-for _ in range(n_iter):
-    new_circ = transpile(init_circ,fake_machine,optimization_level=3)
-    circs.append(new_circ)
-    depths.append(new_circ.depth())
-
-
-fidelities = []
-n_shots = n_iter
-pad_vectors = []
-
-for i in range(n_iter):
-    
-    qubit_pattern = list(circs[i]._layout.get_virtual_bits().values()) # How virtual bits map to physical bits
-    n_phys = len(qubit_pattern) # n of physical bits
-
-
-    perm = [0,1,2,3,4]
-    #   This part still doesn't work perfectly
-    #   It might fail with other machines
-    if (n < 3) :
-        for j in range(n_phys):
-            perm[qubit_pattern[j]] = j
-    else:
-        for op, qubits, clbits in circs[i].data:
-            if op.name == 'measure':
-                perm[qubits[0].index] = clbits[0].index
-    circs[i].remove_final_measurements()
-    circs[i].save_density_matrix()
-    
-    qubit_pattern = perm
-
-    aug_desired_vector = desired_vector
-
-    for k in range(n_phys-n):
-        aug_desired_vector = np.kron([1,0],aug_desired_vector)
-
-
-    perm_circ = Permutation(n_phys, qubit_pattern) # Creating a circuit for qubit mapping
-    perm_unitary = Operator(perm_circ) # Matrix for the previous circuit
-
-    perm_aug_desired_vector = perm_unitary.data @ aug_desired_vector
-    pad_vectors.append(perm_aug_desired_vector)
-
-for i in range(n_shots):
-    result = execute(circs[i],machine_simulator,shots=1).result()
-    noisy_dens_matr = result.data()['density_matrix']
-    fid = state_fidelity(pad_vectors[i],noisy_dens_matr)
-    fidelities.append(fid)
-
+fidelities = getFidelities(circs, machine_simulator, desired_vector)
 mean_fidelity = sum(fidelities)/len(fidelities)
 print("mean fidelity: " + str(mean_fidelity))
 
 plt.figure(figsize=(8, 6))
 plt.hist(fidelities, bins=list(np.arange(0,1.2,0.01)), align='left', color='#AC557C')
 plt.xlabel('Fidelity', fontsize=14)
-plt.ylabel('Counts', fontsize=14);
+plt.ylabel('Counts', fontsize=14)
 plt.show()
 
 
