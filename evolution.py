632315b411df84d4d9b3250ecc38335a91520582
Tom Rindell <tomoskar@protonmail.com>
2021-06-15 15:51:03 +0300

    Moved plotting to tools.py

--- evolution.py
+++ evolution.py
@@ -219,26 +219,10 @@ def geneticAlgorithm(pop, toolbox, NGEN, problemName, problemDescription, epsilo
   size_avgs = logbook.chapters["size"].select("avg")
   print(fit_mins)
   print(size_avgs)
-  import matplotlib.pyplot as plt
 
-  fig, ax1 = plt.subplots()
-  line1 = ax1.plot(gen, fit_mins, "b-", label="Minimum Fitness")
-  ax1.set_xlabel("Generation")
-  ax1.set_ylabel("Fitness", color="b")
-  for tl in ax1.get_yticklabels():
-      tl.set_color("b")
+  from tools import plotGenFit
+  plotGenFit(logbook)
 
-  ax2 = ax1.twinx()
-  line2 = ax2.plot(gen, size_avgs, "r-", label="Average Size")
-  ax2.set_ylabel("Size", color="r")
-  for tl in ax2.get_yticklabels():
-      tl.set_color("r")
-
-  lns = line1 + line2
-  labs = [l.get_label() for l in lns]
-  ax1.legend(lns, labs, loc="center right")
-
-  plt.show()
   if returnLog:
       return pop, logbook
   return pop
--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pickle
 from datetime import datetime
 
@@ -38,3 +39,28 @@ def load(path):
 #   Load allowed gateset, fitness and seed from a file
 def getSetup(path):
     return 0
+
+#   Plot generations and fitness
+def plotGenFit(logbook):
+  gen = logbook.select("gen")
+  fit_mins = logbook.chapters["fitness"].select("min")
+  size_avgs = logbook.chapters["size"].select("avg")
+
+  fig, ax1 = plt.subplots()
+  line1 = ax1.plot(gen, fit_mins, "b-", label="Minimum Fitness")
+  ax1.set_xlabel("Generation")
+  ax1.set_ylabel("Fitness", color="b")
+  for tl in ax1.get_yticklabels():
+      tl.set_color("b")
+
+  ax2 = ax1.twinx()
+  line2 = ax2.plot(gen, size_avgs, "r-", label="Average Size")
+  ax2.set_ylabel("Size", color="r")
+  for tl in ax2.get_yticklabels():
+      tl.set_color("r")
+
+  lns = line1 + line2
+  labs = [l.get_label() for l in lns]
+  ax1.legend(lns, labs, loc="center right")
+
+  plt.show()
